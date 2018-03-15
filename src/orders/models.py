088780c1a1ba72aacf6b4a0295d5d55c6a7a5440
Michael Pavlov <mihpavlov@gmail.com>
2018-03-15 17:21:37 +0530

    adds Order.uuid field

--- src/orders/models.py
+++ src/orders/models.py
@@ -1,3 +1,5 @@
+import uuid as uuid
+
 from django.db import models
 
 
@@ -5,6 +7,7 @@ class Order(models.Model):
     # product = models.ForeignKey(Product)
     quantity = models.IntegerField(null=False, blank=False)
     total_price = models.IntegerField(null=False, blank=False)
+    uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
 
     def __str__(self):
         return "#{}, goods quantity: {}, total price: {}".format(self.id, self.quantity, self.total_price)
