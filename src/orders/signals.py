b7bb0e385fe536e193a92003ec44b0358c9b15f3
Michael Pavlov <mihpavlov@gmail.com>
2018-03-15 17:23:23 +0530

    implements order post_save and post_delete functions

--- src/orders/signals.py
+++ src/orders/signals.py
@@ -1,31 +1,29 @@
-from django.db.models.signals import post_save, post_delete, pre_init, post_init
+import requests
+from django.conf import settings
+from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
+from rest_framework.reverse import reverse
 
+from orders import serializers
 from orders.models import Order
 
+
 @receiver(post_save, sender=Order)
-def order_post_save(sender, instance, created, **kwargs):
-    print('post_save receiver')
-    sender
-    pass
-    # order = instance.as_dict()
-    #
-    # url = settings.MIRROR_URI
-    #
-    # if created:
-    #     url = reverse_lazy("order:create")
-    #     url = mirror_server + url
-    #     requests.post(url, order)
-    # else:
-    #     url = reverse_lazy("order:update")
-    #     url = mirror_server + url
-    #     requests.put(url, order)
+def order_post_save(instance, created, **kwargs):
+    data = serializers.OrderSerializer(instance).data
+    if created:
+        url = reverse("orders:order-list")
+        url = settings.MIRROR_URI + url
+        requests.post(url, data=data)
+    else:
+        url = reverse("orders:order-detail", kwargs={"uuid": data.pop("uuid")})
+        url = settings.MIRROR_URI + url
+        requests.put(url, data=data)
 
-
-@receiver(post_init, sender=Order)
-def order_post_init(sender, instance, created, **kwargs):
-    pass
 
 @receiver(post_delete, sender=Order)
-def order_post_delete(sender, instance, **kwargs):
-    pass
+def order_post_delete(instance, **kwargs):
+    data = serializers.OrderSerializer(instance).data
+    url = reverse("orders:order-detail", kwargs={"uuid": data.pop("uuid")})
+    url = settings.MIRROR_URI + url
+    requests.delete(url)
